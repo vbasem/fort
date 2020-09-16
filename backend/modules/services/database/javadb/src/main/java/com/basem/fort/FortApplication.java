ba902d4aa84c04960454c430ff26cc0bf2144859
Basem Vaseghi <vbasem@gmail.com>
2020-09-16 16:32:28 +0200

    added backend

---
+++ backend/modules/services/database/javadb/src/main/java/com/basem/fort/FortApplication.java
@@ -0,0 +1,13 @@
+package com.basem.fort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortApplication.class, args);
+	}
+
+}
---
+++ backend/modules/services/database/javadb/src/test/java/com/basem/fort/FortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basem.fort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
